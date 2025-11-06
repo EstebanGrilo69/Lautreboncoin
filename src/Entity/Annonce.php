907b5b3e79b442a23258ab495eb6a0ed85c88a39
EstebanGrilo18000 <esteban.grilo@campus.lasallebourges.com>
2025-11-06 16:22:54 +0100

    Initial Commit

---
+++ src/Entity/Annonce.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnonceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnnonceRepository::class)]
+class Annonce
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $Titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Description = null;
+
+    #[ORM\Column]
+    private ?float $Prix = null;
+
+    #[ORM\ManyToOne(inversedBy: 'annonces')]
+    private ?Categorie $Categorie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Photo = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Localisation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->Titre;
+    }
+
+    public function setTitre(string $Titre): static
+    {
+        $this->Titre = $Titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): static
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(float $Prix): static
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->Categorie;
+    }
+
+    public function setCategorie(?Categorie $Categorie): static
+    {
+        $this->Categorie = $Categorie;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->Photo;
+    }
+
+    public function setPhoto(string $Photo): static
+    {
+        $this->Photo = $Photo;
+
+        return $this;
+    }
+
+    public function getLocalisation(): ?string
+    {
+        return $this->Localisation;
+    }
+
+    public function setLocalisation(string $Localisation): static
+    {
+        $this->Localisation = $Localisation;
+
+        return $this;
+    }
+}
---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Titre')
+            ->add('Description')
+            ->add('Prix')
+            ->add('Photo')/*, FileType::class,  [
+                'label'=>'Photo (PDF file)',
+                'mapped'=>false,
+                'requierd'=>false,
+                'constraints'=>[
+                    new File(
+                        maxSize: '1024k',
+                        extensions: ['pdf'],
+                        extensionsMessage: "S'il vous plaît, veuillez placer votre image du produit afin de valider l'annonce, merci"
+                    )
+                ]
+                
+            ])*/
+            ->add('Localisation')
+            ->add('Categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'libelle',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
