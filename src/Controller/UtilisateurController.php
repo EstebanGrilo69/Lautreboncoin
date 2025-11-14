bd0705a27f3fece2473ce79e811c7bb9820d057c
EstebanGrilo69 <esteban.grilo@estiam.com>
2025-11-14 16:05:09 +0100

    tiers_Temps

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -11,6 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use App\Form\RegistrationFormType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
 #[Route('/utilisateur')]
 final class UtilisateurController extends AbstractController
 {
@@ -22,25 +26,38 @@ final class UtilisateurController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_utilisateur_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $utilisateur = new Utilisateur();
-        $form = $this->createForm(UtilisateurType::class, $utilisateur);
-        $form->handleRequest($request);
+#[Route('/new', name: 'app_utilisateur_new', methods: ['GET', 'POST'])]
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($utilisateur);
-            $entityManager->flush();
+public function utilisateur(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+{
+     $user = new Utilisateur();
+    $form = $this->createForm(UtilisateurType::class, $user);
+    $form->handleRequest($request);
 
-            return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
+    if ($form->isSubmitted() && $form->isValid()) {
+        /** @var string $plainPassword */
+        $plainPassword = $form->get('plainPassword')->getData();
+
+        // encode the plain password
+        $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+        // Récupérer le rôle depuis le formulaire
+        $role =$form ->get('roles')->getData();
+        $user->setRoles([$role]);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // do anything else you need here, like send an email
+        return $this->redirectToRoute('app_utilisateur_index');
         }
 
-        return $this->render('utilisateur/new.html.twig', [
-            'utilisateur' => $utilisateur,
-            'form' => $form,
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form,
         ]);
     }
+
+
 
     #[Route('/{id}', name: 'app_utilisateur_show', methods: ['GET'])]
     public function show(Utilisateur $utilisateur): Response
